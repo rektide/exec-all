f81718822b1cbc17e636a1bd37834599e691e789
rektide de la faye <rektide@voodoowarez.com>
2025-12-05 21:18:54 -0500

    build out
    but still a bug by way of how tinyexec/node only let stdout get used once

--- exec-all.ts
+++ exec-all.ts
@@ -1,42 +1,79 @@
+#!/usr/bin/env node
 import process from "node:process";
-import { x } from "tinyexec";
+import { type Output, type Result, x } from "tinyexec";
 import { tokenizeArgs } from "args-tokenizer";
 
 /**
  * Promise.race() that returns the `promise` and `index` of the promise that won the race, as well as the `value`
  */
-function raceWithIndex<T>(promises: Iterable<Promise<T>>) {
+function raceWithIndex(promises: Iterable<Result>): Promise<
+  & {
+    promise: Result;
+    index: number;
+  }
+  & (
+    | { value: Output; error?: undefined }
+    | { value?: undefined; reason: any }
+  )
+> {
   // Map each promise with a wrapper that captures the `promise` and `index` while forwarding the `value`.
-  // If the promise throws, instead of a `value` there will be an `error`
-  const mapped;
+  // If the promise throws, instead of a `value` there will be an `reason`
+  const mapped = promises.map((promise, index) =>
+    promise.then(
+      (value) => ({ promise, index, value }),
+      (reason) => ({ promise, index, reason }),
+    )
+  );
 
   return Promise.race(mapped);
 }
 
-export function* raceCmds(cmds = process.argv.slice(2)) {
+export async function* raceCmds(cmds: string[] = process.argv.slice(2)) {
   // launch cmds phase
   // use arg-tokenizer to parse each cmd into their component tokens
-  let tokenized;
   // map each use tinyexec to launch each cmd with it's arguments, saving each promise into
-  let running;
+  const remaining = cmds.map((cmd: string) => {
+    const [command, ...args] = tokenizeArgs(cmd);
+    return x(command, args);
+  });
 
   // stream results phase
-  let remaining = running;
   // loop through remaining cmd
   while (remaining.length) {
     // race the remaining
-    const winner = raceWithIndex(remaining);
+    const winner = await raceWithIndex(remaining);
     // remove the `winner` from `remaining` via it's `index`
+    remaining.splice(winner.index, 1);
     // yield the winner
     yield winner;
   }
 }
 
-export function execAll(cmds = process.argv.slice(2)) {
+export async function execAll(cmds: string[] = process.argv.slice(2)) {
+  let firstCmd = true;
   for await (let cmd of raceCmds(cmds)) {
     // empty console.error if this is not the first cmd to return, to add visual-separation
+    if (!firstCmd) {
+      console.error();
+      firstCmd = false;
+    }
+
     // console.error with the cmd.process.spawnfile so the user can tell
+    console.error(`  --> ${cmd.promise.process?.spawnargs?.join(" ")}`);
+
     // actual results
-    console.log(cmd.promise.stdout);
+    if ("reason" in cmd) {
+      console.error(cmd.reason);
+    } else {
+      // we need a way to record and reassemble both streams in order
+      console.log(cmd.value.stdout);
+      if (cmd.value.stderr) {
+        console.error(cmd.value.stderr);
+      }
+    }
   }
+}
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+  execAll();
 }
